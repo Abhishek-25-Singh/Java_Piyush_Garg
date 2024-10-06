25acfe1976652d0fa9c72af8592334c833efb416
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-10-06 13:15:57 +0530

    EvenOrOdd Using Modulo Operator (Speed Is Slow)

---
+++ 1_Introduction to DSA/Number_System/EvenOrOdd.java
@@ -0,0 +1,12 @@
+public static void main (String [] args){
+        System.out.print("Enter Integer :");
+        Scanner Scanner = new Scanner(System.in);
+        int INT_Num = Scanner.nextInt();
+
+        //Slow Code Using Decimal/Arithmetic operation
+//        if( INT_Num % 2 == 0 ){
+//            System.out.println("The Integer "+INT_Num+" is Even");
+//        }else{
+//            System.out.println("The Integer "+INT_Num+" is odd");
+//        }
+    }
