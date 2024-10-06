7462ca7a5c2a13f8215a933cf32fead723979513
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-10-06 14:09:07 +0530

    Bitwise Operators
    📌Bitwise And operator(&)
    📌Bitwise OR operator(|)
    📌Bitwise NOT operator(~)
                 📍Step 1 : Binary Representation of the Decimal Number
                 📍step 2 :  Apply Not Operator
                 📍step 3 :  flip All the bits
                 📍step 4 :  Add 1
    📌Bitwise XOR operator(^)

---
+++ 1_Introduction to DSA/BitwiseOperator/Bitwise.java
@@ -0,0 +1,26 @@
+public static void main(String[] args){
+
+//       int a = 3;
+//       int b = 4;
+//        Bitwise And operator(&)
+//        System.out.println(" a & b : "+ (a & b));
+//        //Bitwise OR operator(|)
+//        System.out.println(" a | b : "+ ( a | b));
+//        //Bitwise NOT operator(~)
+//        System.out.println(" ~a :" + ( ~a));
+//        //Bitwise XOR operator(^)
+//        System.out.println( " a ^ b : "+ (a ^ b)) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
