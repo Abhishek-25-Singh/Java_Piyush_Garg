f0bb81aefacb36b8de79953536ac4276d11410d3
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-10-06 12:34:38 +0530

    Creating Integer
                   👉 Creating Integer To store Decimal Number 👈
    1.Just Print the Text (Enter The Decimal Number For Conversion:)
    2.Scanner is created to read input
    3.Read the decimal number from user input
    4.Store the original decimal number for final output

--- 1_Introduction to DSA/Number_System/DecimalToBinary.java
+++ 1_Introduction to DSA/Number_System/DecimalToBinary.java
@@ -1,6 +1,7 @@
  public static void main (String[] args){
-
-
+System.out.print("Enter The Decimal Number For Conversion:");
+      Scanner Scanner = new Scanner(System.in);
+      int Decimal_Number = Scanner.nextInt();
 
 
 
