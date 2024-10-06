7f4d4eca4e6ce39a0266fb1451bee80943f9443d
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-10-06 16:50:08 +0530

    Hamming Weights Question
    Write a Function That Takes an unsigned integer and return the number of 1bits it has(also Known as Hamming Weights)

--- 1_Introduction to DSA/BitwiseOperator/Bitwise.java
+++ 1_Introduction to DSA/BitwiseOperator/Bitwise.java
@@ -14,6 +14,20 @@ public static void main(String[] args){
 //        System.out.println(10>>1);
 //        //Bitwise left Shift Operator
 //        System.out.println(10<<2);
+//Give an integer , return an array ans of length n+1 such That for each i(0<=i<=n) is the number of 1's in the binary representation of i
+        System.out.print("Enter the integer :");
+       Scanner Scanner = new Scanner(System.in);
+       int num = Scanner.nextInt();
+       int result = 0; // This will hold the count of '1' bits
+        while(num !=0){
+            int last_bit = num & 1; // Get the last bit of num
+            if(last_bit==1){
+                result++; // If the last bit is 1, increment the result
+            }
+            num = num >> 1; // Right shift num to check the next bit
+        }
+        System.out.println("Number of 1 bits: " + result);
+    }
 
 
 
@@ -26,4 +40,3 @@ public static void main(String[] args){
 
 
 
-}
